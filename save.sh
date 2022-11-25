236512ee275058065816337cb07ab77b04cddca7
Derek <dcsmith121244@gmail.com>
2022-11-25 22:36:24 +0000

    New update to wiki

--- save.sh
+++ save.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-git add -A
-git commit -m "New update to wiki"
-git push
+while :
+do
+	if [ -n "$(git status --porcelain)" ]
+	then
+		git add -A
+		git commit -m "New update to wiki"
+		git push
+	fi
+
+	sleep 60
+done
